9fd04773ea5f8eb505171a357e21f114cf02d9b3
Ganz228 <viktorsinkov16@gmail.com>
2023-10-24 00:32:46 +0900

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammPosl
+{
+    
+    class Program
+    {
+        private static double x;
+        private static double y;
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x = ");
+            double x = Convert. ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+
+            double a = (14 * Math.Sin(x) + Math.Pow(y, 2)) / (Math.Pow(Math.Cos(x), 3));
+            
+            Console.WriteLine(a);
+
+
+
+        }
+    }
+}
+
